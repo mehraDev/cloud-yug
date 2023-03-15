268b5a28c0acad43064e3e328e0608141f52953e
mehraDev <126305156+mehraDev@users.noreply.github.com>
2023-03-15 05:56:45 +0530

    Chore: Adding redux

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,16 +1,42 @@
 
 import styled from 'styled-components'
+import Avatar from './Avatar'
+import Logo from './Logo/Logo'
 
-const HeaderWrapper = styled.div`
-    height: 64px;
-    padding-inline: 50px;
-    color: rgba(0, 0, 0, 0.88);
-    line-height: 64px;
-    background: #001529;
-`
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const Header = () => {
-  return <HeaderWrapper/>
+  return <HeaderWrapper>
+    <MainHeading>
+        <Logo/>
+        <ShopTitle>La Lavash</ShopTitle>
+    </MainHeading>
+    <HeaderMenu>
+        <Avatar/>
+    </HeaderMenu>
+  </HeaderWrapper>
 }
 
+const HeaderWrapper = styled.div`
+    padding-inline: 1rem;
+    line-height: 64px;
+    background: #001529;
+    display:flex;
+    color: white;
+    justify-content: space-between;
+    
+`
+const HeaderMenu = styled.div` 
+display:flex;
+align-items: center;
+justify-content: space-between;
+`
+const ShopTitle = styled.div` 
+margin: 0 .5rem;
+`
+const MainHeading = styled.h2` 
+display:flex;
+align-items: center;
+justify-content: space-between;
+`
 export default Header
