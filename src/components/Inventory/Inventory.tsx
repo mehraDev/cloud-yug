d1d48b023e3d93f9f72868d480ddd22a952d70af
mehraDev <126305156+mehraDev@users.noreply.github.com>
2023-03-21 18:15:34 +0530

    Adding sample commit

--- src/components/Inventory/Inventory.tsx
+++ src/components/Inventory/Inventory.tsx
@@ -1,54 +1,81 @@
 /* eslint-disable react/jsx-key */
-import { QRCode } from 'antd'
+import { Button } from 'antd'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import ItemViewer from '../ItemViewer/ItemViewer'
-
+import { getItems, auth } from '../../firebase/firebase'
+import Item from '../common/Item/Item'
+import ItemViewer from '../common/ItemViewer/ItemViewer'
+import { Search } from '../common/Search/'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-const Inventory = () => {
+const ImageDB = () => {
+  const TITLE = 'Inventory'
+  const SHOP_ID = 'P3nv9Fi1TJvlVi6ToYez'
+  const [itemsList, setItemsList] = useState([])
+  const [query, setQuery] = useState('')
+  const [user, setUser] = useState(null)
+  const [filteredList, setFilteredList] = useState([])
+  const createUser = () => {
+  }
+
+  useEffect(() => {
+    let ignore = false
+    createUser()
+    getItems(SHOP_ID).then(result => {
+      if (!ignore) {
+        setItemsList(result)
+      }
+    })
+    return () => {
+      ignore = true
+    }
+  }, [])
+
+  const items = query ? itemsList.map(item => (item.name.toLowerCase().includes(query.toLowerCase()) ? <Item data={item}/> : null)) : itemsList.map(item => <Item data={item}/>)
   return <Card>
     <CardHeading>
-      <div>Inventory</div>
+      <h3>{TITLE}</h3>
+      <Search onSearch={setQuery}/>
       <ControlBar>
-
+        <Button >Edit</Button>
       </ControlBar>
     </CardHeading>
     <CardBody>
-    <ItemViewer items={[<QRCode value="https://ant.design/" />, <QRCode value="https://ant.design/" />]}/>
+    <ItemViewer items={items}/>
     </CardBody>
   </Card>
 }
 
-const Item = styled.div`
- background-color: green;
- height: 300px;
- width: 300px;
- border-radius: 3px  3px 0px;
- margin: 4px;
-`
 // @@ padding sepration for mobile/desktop
 const Card = styled.div`
-background-color: white;
 height: 100%;
-border-radius: 5px;
 position: relative;
 `
 const CardBody = styled.div`
-
+height: 100%;
+margin: 1rem;
+overflow: auto;
+overflow-y: scroll;
+position: relative;
 `
-const CardHeading = styled.h3`
+const CardHeading = styled.div`
+background-color: white;
 text-align: left;
 justify-content: space-between;
 display: flex;
-padding: .7rem;
+align-items: center;
+padding: 4px 36px;
+    padding-right: 10rem;
 border-bottom: 1px solid rgba(5, 5, 5, 0.06);
+h3{
+  font-weight:500;
+}
 `
 const ControlBar = styled.div`
 text-align: left;
 margin: 4px;
-padding: 2px 4px;
-background: red;
 border-bottom: 1px solid rgba(5, 5, 5, 0.06);;
 `
 
-export default Inventory
+export default ImageDB
